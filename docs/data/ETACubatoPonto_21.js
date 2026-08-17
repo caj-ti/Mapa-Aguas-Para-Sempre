d2bcb62388fe7dc46a5e93b2317d577e87434b5e
caj-ti <ti@aguasdejoinville.com.br>
2026-08-17 09:38:02 -0300

    att

--- docs/data/ETACubatoPonto_21.js
+++
@@ -1,1 +0,0 @@
-var json_ETACubatoPonto_21 = {"type":"FeatureCollection","name":"ETACubatoPonto_21","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[-48.939363759940719,-26.181020914385282]}}]}
